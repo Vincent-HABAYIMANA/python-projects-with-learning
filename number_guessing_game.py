a53ad358672c7069cf8cfd23331628a2b57be186
Vincent-HABAYIMANA <vincenthabayimana1@gmail.com>
2026-05-03 01:10:40 +0200

    Initial commit for second learning  project updated

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -3,7 +3,6 @@ import random
 play = True
 while  True:
  number_to_guess=random.randint(1,100)
-#loop
  try:
    guess = int(input ('enter your guessing number between 1 and 100: '))
    if guess < number_to_guess:
